ea5b33835bb7077b8d54cb13973e1239238028e0
endo-kotaro <57199117+endo-kotaro@users.noreply.github.com>
2020-08-12 13:07:48 +0900

    Feat:Heroku_deploy (#37)
    * Modify README.md (#27)
    
    * complete user-test
    
    * Revert "complete user-test"
    
    This reverts commit 71d573cffffef8828ef0ed04c551b41f147a0f0c.
    
    * Feat: add user model validation (#21)
    
    * feat: Add validations to user model
    
    * feat: add unique index to email column
    
    * user_models test
    
    * Revert "Feat: add user model validation (#21)"
    
    This reverts commit e049017353c198ba61c1151eaaa4f2998b18a328.
    
    * Revert "Revert "Feat: add user model validation (#21)""
    
    This reverts commit b9166649d9b2656b30ded80923442d472b716da5.
    
    * fix: remove user_model spec
    
    * user_model test (#26)
    
    * Feat/user test request (#31)
    
    * test
    
    * Userリクエストのテストを実装
    
    * database.yml修正, users_spec.rbを修正
    
    * describe 'Patch/updatedoを修正
    
    * database.ymlとseeds.rb修正
    
    * Feat: scaffold micropost files (#33)
    
    * feat: scaffold microposts files
    
    * feat: add micropost content validation
    
    * feat: correct rubocop errors
    
    * Feat: Micropost request test && belongs to User (#35)
    
    * user_model test
    
    * micropost request test && belongs to model
    
    * request test && belongs to User
    
    * request test && belongs to user
    
    * Feat/micropost test (#36)
    
    * test
    
    * Rspecで2つのFailuresが発生
    
    * micropost.rbにuser_idのバリデーションを追加
    
    Co-authored-by: Kenta Katsumata <polyglot.software@gmail.com>
    Co-authored-by: K-kind <55728594+K-kind@users.noreply.github.com>
    Co-authored-by: K.K <gokennsirou@yahoo.co.jp>
    Co-authored-by: Yori-goreng <61875210+Yori-goreng@users.noreply.github.com>

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :content,
+            presence: true,
+            length: { maximum: 140 }
+  validates :user_id, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  validates :name,
+            presence: true,
+            length: { minimum: 2, maximum: 12 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
